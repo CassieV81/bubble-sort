ec37a53c5cc68350697792bb87b426baa5a77461
CassieV <obicassievirtue@gmail.com>
2022-07-20 14:00:55 +0200

    Remove unnecessary variable

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,7 +1,7 @@
 
 def bubble_sort(num_array)
   for i in 0..num_array.length do
-    sorted_array = num_array.each_with_index do |num, index|
+    num_array.each_with_index do |num, index|
       if (index + 1) == num_array.length
         num_array
       elsif num_array[index] > num_array[index + 1]
@@ -9,7 +9,7 @@ def bubble_sort(num_array)
       end
     end
   end
-  p sorted_array
+  p num_array
 end
 
 bubble_sort([4,3,78,2,0,2,5,65,900,87,456,21,32,1,0,9])
