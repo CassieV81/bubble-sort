48035163a23cf6ca64b62edca2fe0972b583d819
CassieV <obicassievirtue@gmail.com>
2022-07-22 15:05:25 +0200

    Replace for..in iterator with each... Rubocop suggested using each and autocorrected the num array

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,7 +1,8 @@
+# Bubble Sort
 
 def bubble_sort(num_array)
-  for i in 0..num_array.length do
-    num_array.each_with_index do |num, index|
+  num_array.each do
+    num_array.each_with_index do |_num, index|
       if (index + 1) == num_array.length
         num_array
       elsif num_array[index] > num_array[index + 1]
@@ -12,7 +13,6 @@ def bubble_sort(num_array)
   p num_array
 end
 
-bubble_sort([4,3,78,2,0,2,5,65,900,87,456,21,32,1,0,9])
+bubble_sort([4, 3, 78, 2, 0, 2, 5, 65, 900, 87, 456, 21, 32, 1, 0, 9])
 
 # => [0, 0, 1, 2, 2, 3, 4, 5, 9, 21, 32, 65, 78, 87, 456, 900]
-
